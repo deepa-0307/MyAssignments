82dc303e8337e0bdba5ea735da8ef34cee160a03
deepa-0307 <deepa.m2info@gmail.com>
2025-02-10 00:04:59 +0530

    Add files via upload
    Week1 day1 assignment completed

---
+++ day1/assignment/Chrome.java
@@ -0,0 +1,19 @@
+package week1.day1.assignment;
+
+public class Chrome {
+	public static void main (String[] args) {
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		System.out.println("Float datatype: "+version);
+		System.out.println("String datatype: "+developer);
+		System.out.println("Boolean datatype: "+isBeta);
+		System.out.println("Int datatype: "+releaseYear);
+		System.out.println("Char datatype: "+shortcutKey);
+	
+	}
+
+}
+
